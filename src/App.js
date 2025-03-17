c35b2d16eb2a6107ebedeb629f2e6ce5a617d29d
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-03-17 13:02:30 -0300

    calculadora-init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import logo from './imagenes /logo.png';
+
+
 
 function App() {
   return (
     <div className='App'>
-     <div>
+
+      <div className= 'logo-contenedor'>
+        <img src={logo} className='logo' alt='Logo de la aplicación'/>
+        <h1>CALCULADORA INTERACTIVA</h1>
+      </div>
+
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+
+        </div>
 
 
-     </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+
+      </div>
+
+
     </div>
   );
 }
