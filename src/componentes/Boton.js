bc61576da8a581dbd276ed1fe322ca0ea9c0697c
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-03-17 14:48:39 -0300

    avances falta funcionalidad

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import '../hojas-de-estilos/Boton.css';
 
 function Boton(props) {
 
   const esOperador = valor => {// Función que determina si el valor es un operador
     return isNaN(valor) && (valor !== '.') && (valor != '=');
+  };
 
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`.trimEnd()}>
       {props.children}
     </div>
   );
